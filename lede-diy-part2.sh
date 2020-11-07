2905bbbbdffee927628bb4d798a6d70d0d895812
Kikoch <61268957+Kikoch@users.noreply.github.com>
2020-11-07 23:41:20 +0800

    Update lede-diy-part2.sh

--- lede-diy-part2.sh
+++ lede-diy-part2.sh
@@ -13,4 +13,15 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="Kikoch"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"Kikoch"@' .config
 
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+#=================================================
+# Modify the version number
+sed -i "s/OpenWrt /Kikoch build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
